3f439e4a6d53d52488d43fb8cf343c7edef25401
plopeziv <plopez@8thlight.com>
2022-03-24 08:33:05 -0500

    Add the ability to create Games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,4 +6,25 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
   end
+
+  def new
+    @game = Game.new
+  end 
+
+  def create
+    @game = Game.new(
+      gameId: "...",
+      teamOne: "...",
+      teamTwo: "...",
+      gameSpread: "...",
+      status: "...",
+      scoreDiff: "..."
+    )
+
+    if @game.save
+      redirect_to @game
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
