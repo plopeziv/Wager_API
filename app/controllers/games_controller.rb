d05e574e064e452f74d43068503bdec900e65156
plopeziv <plopez@8thlight.com>
2022-03-24 13:39:04 -0500

    Add delete to CRUD features

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -35,6 +35,13 @@ class GamesController < ApplicationController
     end
   end
 
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+
+    redirect_to games_path, status: :see_other
+  end
+
   private
     def game_params
       params.require(:game).permit(
