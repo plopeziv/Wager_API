71826bb7b03be2ec5520045875a5c6bf0f4593f6
plopeziv <plopez@8thlight.com>
2022-03-24 10:41:31 -0500

    Validation for saved forms

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,8 @@
 class Game < ApplicationRecord
+    validates :gameId, presence: true, length: {minimum: 6}, uniqueness: true
+    validates :teamOne, presence: true, length: {minimum: 3}
+    validates :teamTwo, presence: true, length: {minimum: 3}
+    validates :gameSpread, presence: true, numericality:true
+    validates :status, inclusion: [true, false]
+    validates :scoreDiff, presence: false, numericality: {only_integer: true}, allow_nil: true
 end
